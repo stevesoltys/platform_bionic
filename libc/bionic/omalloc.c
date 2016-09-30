ae79d990fe1c2adfe7b87de96738c7c5e86e7b71
Daniel Micay <danielmicay@gmail.com>
2016-09-30 13:09:39 -0400

    add __dynamic_object_size
    Change-Id: I05037ec20fe1808798b6d24438b5f009028a78f9

--- libc/bionic/omalloc.c
+++ libc/bionic/omalloc.c
@@ -2023,6 +2023,62 @@ o_malloc_usable_size(const void *p)
 	return ret;
 }
 
+static size_t
+omalloc_object_size(const void *p)
+{
+	struct dir_info *pool = getpool();
+	struct region_info *r;
+	size_t sz;
+
+	r = find(pool, (void *)p);
+	if (r == NULL)
+		return (size_t)-1;
+
+	REALSIZE(sz, r);
+
+	if (sz == 0)
+		return sz;
+
+	if (sz <= MALLOC_MAXCHUNK) {
+		uintptr_t base = (uintptr_t)p & ~(sz - 1);
+		size_t offset = (uintptr_t)p - base;
+		return sz - mopts.malloc_canaries - offset;
+	}
+
+	uintptr_t base = (uintptr_t)p & ~MALLOC_PAGEMASK;
+	if (mopts.malloc_move &&
+	    sz - mopts.malloc_guard < MALLOC_PAGESIZE -
+	    MALLOC_LEEWAY) {
+		base = base + ((MALLOC_PAGESIZE - MALLOC_LEEWAY -
+		    (sz - mopts.malloc_guard)) & ~(MALLOC_MINSIZE-1));
+	}
+
+	size_t offset = (uintptr_t)p - base;
+
+	if (offset > sz - mopts.malloc_guard)
+		return 0; /* abort? */
+
+	return sz - mopts.malloc_guard - offset;
+}
+
+size_t
+o___malloc_object_size(const void *p)
+{
+	size_t ret;
+	struct dir_info *d;
+
+	_MALLOC_LOCK();
+	d = getpool();
+	if (d == NULL) {
+		_MALLOC_UNLOCK();
+		return (size_t)-1;
+	}
+	d->func = "__malloc_object_size():";
+	ret = omalloc_object_size(p);
+	_MALLOC_UNLOCK();
+	return ret;
+}
+
 struct mallinfo
 o_mallinfo() {
 	struct mallinfo mi;
