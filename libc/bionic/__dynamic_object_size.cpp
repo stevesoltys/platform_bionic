7af087fee12c0983fae3852027d282c23ea3684c
Daniel Micay <danielmicay@gmail.com>
2016-11-01 17:32:17 -0400

    add __dynamic_object_size executable fast path
    Change-Id: I2f0edde39583f930e675d3072096c68bc7b1c9f9

--- libc/bionic/__dynamic_object_size.cpp
+++ libc/bionic/__dynamic_object_size.cpp
@@ -1,5 +1,8 @@
+#include <link.h>
+#include <elf.h>
 #include <stddef.h>
 #include "private/bionic_globals.h"
+#include "private/bionic_page.h"
 #include "pthread_internal.h"
 
 extern "C" size_t __malloc_object_size(const void*);
@@ -34,11 +37,68 @@ extern "C" size_t __dynamic_object_size(const void* ptr) {
     return static_cast<char*>(stack_top) - static_cast<char*>(const_cast<void*>(ptr));
   }
 
+  if (ptr > __libc_globals->executable_start && ptr < __libc_globals->executable_end) {
+    return static_cast<char*>(__libc_globals->executable_end) - static_cast<char*>(const_cast<void*>(ptr));
+  }
+
   return __malloc_object_size(ptr);
+}
+
+static size_t phdr_table_get_load_size(const ElfW(Phdr)* phdr_table, size_t phdr_count,
+                                       ElfW(Addr)* out_min_vaddr,
+                                       ElfW(Addr)* out_max_vaddr) {
+  ElfW(Addr) min_vaddr = UINTPTR_MAX;
+  ElfW(Addr) max_vaddr = 0;
+
+  bool found_pt_load = false;
+  for (size_t i = 0; i < phdr_count; ++i) {
+    const ElfW(Phdr)* phdr = &phdr_table[i];
+
+    if (phdr->p_type != PT_LOAD) {
+      continue;
+    }
+    found_pt_load = true;
+
+    if (phdr->p_vaddr < min_vaddr) {
+      min_vaddr = phdr->p_vaddr;
+    }
+
+    if (phdr->p_vaddr + phdr->p_memsz > max_vaddr) {
+      max_vaddr = phdr->p_vaddr + phdr->p_memsz;
+    }
+  }
+  if (!found_pt_load) {
+    min_vaddr = 0;
+  }
+
+  min_vaddr = PAGE_START(min_vaddr);
+  max_vaddr = PAGE_END(max_vaddr);
+
+  if (out_min_vaddr != nullptr) {
+    *out_min_vaddr = min_vaddr;
+  }
+  if (out_max_vaddr != nullptr) {
+    *out_max_vaddr = max_vaddr;
+  }
+  return max_vaddr - min_vaddr;
+}
+
+static int callback(struct dl_phdr_info *info, size_t, void* data) {
+  void* addr = reinterpret_cast<void*>(info->dlpi_addr);
+  if (addr == nullptr) {
+    return 0;
+  }
+  size_t size = phdr_table_get_load_size(info->dlpi_phdr, info->dlpi_phnum, nullptr, nullptr);
+  libc_globals* globals = static_cast<libc_globals*>(data);
+  globals->executable_start = addr;
+  globals->executable_end = static_cast<char*>(addr) + size;
+  return 1;
 }
 
 void __libc_init_dynamic_object_size(libc_globals* globals) {
   globals->enable_object_size_checks = true;
+
+  dl_iterate_phdr(callback, globals);
 
   void* current_base;
   size_t current_size;
