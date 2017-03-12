61a3cfb64d09a875099bddf39861b45bbf125c12
Daniel Micay <danielmicay@gmail.com>
2017-03-12 18:00:01 -0400

    on 64-bit, zero the leading heap canary byte
    This reduces entropy of the canary from 64-bit to 56-bit in exchange for
    mitigating non-terminated C string overflows.
    
    Change-Id: I851934df7fc5b34e830fb67603fa28e5883061a1

--- libc/bionic/omalloc.c
+++ libc/bionic/omalloc.c
@@ -135,6 +135,12 @@ static void _MALLOC_UNLOCK(int mutex)
 #define SOME_FREEJUNK		0
 #endif
 
+#if __LP64__
+#define CANARY_MASK (~(uintptr_t)0xff)
+#else
+#define CANARY_MASK (~(uintptr_t)0)
+#endif
+
 #define MMAP(sz)	mmap(NULL, (sz), PROT_READ | PROT_WRITE, \
     MAP_ANON | MAP_PRIVATE, -1, 0)
 
@@ -1225,7 +1231,7 @@ malloc_bytes(struct dir_info *d, size_t size, __unused void *f)
 	if (mopts.malloc_canaries && bp->size > 0) {
 		char *end = (char *)bp->page + k + bp->size;
 		uintptr_t *canary = (uintptr_t *)(end - mopts.malloc_canaries);
-		*canary = mopts.malloc_chunk_canary ^ hash_chunk(canary);
+		*canary = (mopts.malloc_chunk_canary ^ hash_chunk(canary)) & CANARY_MASK;
 	}
 
 	if (mopts.malloc_junk_init && bp->size > 0)
@@ -1247,7 +1253,7 @@ find_chunknum(struct dir_info *d, struct region_info *r, void *ptr)
 	if (mopts.malloc_canaries && info->size > 0) {
 		char *end = (char *)ptr + info->size;
 		uintptr_t *canary = (uintptr_t *)(end - mopts.malloc_canaries);
-		if (*canary != (mopts.malloc_chunk_canary ^ hash_chunk(canary)))
+		if (*canary != ((mopts.malloc_chunk_canary ^ hash_chunk(canary)) & CANARY_MASK))
 			wrterror(d, "chunk canary corrupted", ptr);
 	}
 
